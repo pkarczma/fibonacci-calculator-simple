89010bf8d274f46e6d770aa19310067e01f37603
Przemyslaw Karczmarczyk <przemyslaw.karczmarczyk@gmail.com>
2025-03-11 11:57:54 +0100

    Change main page text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed
+          I did a change in main branch
         </p>
         <a
           className="App-link"
